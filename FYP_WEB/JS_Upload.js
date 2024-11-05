3962d4883b8929724139443eb245fd49f2f10006
VISHAKA PAHUJA <107913951+VISHAKAPAHUJA@users.noreply.github.com>
2024-11-04 21:54:58 -0800

    Delete FYP_WEB directory

--- FYP_WEB/JS_Upload.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.getElementById('uploadButton').addEventListener('click', function() {
-    document.getElementById('resumeInput').click();
-});
-
-document.getElementById('resumeInput').addEventListener('change', function(event) {
-    const fileName = event.target.files[0] ? event.target.files[0].name : 'No file chosen';
-    alert('You selected: ' + fileName);
-});
